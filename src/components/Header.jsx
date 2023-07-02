4197dbc875b6d752ed5c52cb8305f253b48fb938
WizardMode <humanspacebolt@live.com>
2023-07-01 22:23:01 -0700

    Layout and Outlet

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+        <Link className="site-logo" to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/vans">Vans</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+    )
+}
+
+export default Header
