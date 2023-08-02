f8846b07ef0c2c1a63fb5ccf1f0ca0c38800d555
WizardMode <humanspacebolt@live.com>
2023-08-02 16:14:17 -0700

    happy path(FDCP)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,19 @@
 import React from "react"
 import { useNavigate, useLocation } from "react-router-dom"
+import { loginUser } from "../api"
+
+/* hook up our form so it (halfway) works.
+ * 
+ * 1. Pull in the `loginUser` function from the api.js file
+ * 2. Call loginUser when the form is submitted and log the
+ *    data that comes back. Use "b@b.com" as the username and
+ *    "p123" as the password.
+ * 
+ *    NOTE: loginUser returns a promise, so you'll need
+ *    a .then(data => {...}) to access the data, or use
+ *    a separate aync function defined inside handleSubmit
+ * 3. TBA
+ */ 
 
 function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: ""})
@@ -8,7 +22,9 @@ function Login() {
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        loginUser(loginFormData)
+            .then(data => console.log(data))
+        // console.log(loginFormData)
     }
 
     function handleChange(e) {
