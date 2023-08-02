3f1cccba7acacec59fd494c9b4c7b5246812e59e
WizardMode <humanspacebolt@live.com>
2023-08-02 16:35:31 -0700

    Authentication setup- sad path(FDCP)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,15 +15,44 @@ import { loginUser } from "../api"
  * 3. TBA
  */ 
 
+/**
+ * Code the sad path 🙁
+ * 
+ * 3. Add a `status` state and default it to "idle". When the 
+ *    login form begins submitting, set it to "submitting". When
+ *    it's done submitting and the data has come back, set it 
+ *    to "idle" again
+ * 4. Disable the button when the `status` state is "submitting"
+ *    (this may require some Googling if you haven't done this
+ *    before).
+ * 5. Add an `error` state and default it to `null`. When the
+ *    form is submitted, reset the errors to `null`. If there's
+ *    an error from `loginUser` (add a .catch(err => {...}) in 
+ *    the promise chain), set the error to the error that 
+ *    comes back.
+ * 6. Display the error.message below the `h1` if there's ever
+ *    an error in state
+ */
+
 function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: ""})
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
     const location = useLocation()
     // console.log(location)
 
     function handleSubmit(e) {
         e.preventDefault()
+        setStatus("submitting")
         loginUser(loginFormData)
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                setError(null)
+            }).catch(err => {
+                setError(err)
+            }).finally(() => {
+                setStatus("idle")
+            })
         // console.log(loginFormData)
     }
 
@@ -44,9 +73,13 @@ function Login() {
         <div className="login-container">
             {
                 location.state?.message &&
-                <h3 className="login-first">{location.state.message}</h3>
+                <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
+            {
+                error?.message &&
+                <h3 className="login-error">{error.message}</h3>
+            }
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -62,7 +95,14 @@ function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button 
+                    disabled={status === "submitting"}
+                >
+                    {status ==="submitting"
+                        ? "Logging in..."
+                        : "Log in"
+                    }
+                </button>
             </form>
         </div>
     )
